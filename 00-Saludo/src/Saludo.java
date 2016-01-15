3eee1aaef302886d96645f2a64925a8b5e93026d
pgarciab22 <gbpaty18@gmail.com>
2016-01-15 14:11:43 -0600

    00- Saludo

---
+++ 00-Saludo/src/Saludo.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+ /**
+  * 
+  * @author Patricia García Baeza
+  *
+  */
+
+public class Saludo { 
+ /**
+  * 
+  * @param metodo main que recibe una cadena de caracteres
+  */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"Hola Paty"); //ventana en forma grafica
+		System.out.println("Hola Pato, desde la consola"); //En la consola
+	}
+	
+
+}

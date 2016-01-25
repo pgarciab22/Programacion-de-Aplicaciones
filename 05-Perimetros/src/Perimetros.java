1e16c1e5a77dc1554236088d48b8391bd946760e
pgarciab22 <gbpaty18@gmail.com>
2016-01-25 00:11:38 -0600

    05-Perimetros

---
+++ 05-Perimetros/src/Perimetros.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author Patricia García Baeza
+ *
+ */
+
+public class Perimetros {
+/**
+ * 
+ * @param lado Recibe un numnero entero
+ * @return regresa la suma de 4 numeros
+ */
+	public int cuadrado(int lado){
+		
+		return lado + lado + lado + lado;
+	}
+
+}
